3ea67fa6a24616505532ab86048f06aaaf4dd126
Alphaharrius <47113671+Alphaharrius@users.noreply.github.com>
2020-08-29 02:34:52 +0800

    [u-conv] implementation of a faster approach

--- utils/u-conv.h
+++ utils/u-conv.h
@@ -1,9 +1,15 @@
 #ifndef U_CONV_H
 #define U_CONV_H
 #pragma once
+/**
+ * Copyright (c) 2019, 2020, Alphaharrius. All rights reserved.
+ * 
+ * u-conv.h
+ */
 
 #include "../core/v-type.h"
+#include <string.h>
 
-u8 Util_QuickUnsignedConvert(u8 *str, u64 val);
+u8 Util_QuickUnsignedConvert(u8 *str, u64 val, u8 disp, u8 pad);
 
 #endif
